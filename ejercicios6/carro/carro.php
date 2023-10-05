e835734c56e4c36573e8c9b50cf7bdfe2d0c163f
Adrián Primo <adrpriber@alu.edu.gva.es>
2023-10-05 16:57:50 +0200

    Terminado actividad sesión de carro

--- ejercicios6/carro/carro.php
+++ ejercicios6/carro/carro.php
@@ -1,3 +1,40 @@
+<?php
+session_start();
+
+$articulos = array(
+    array("id" => 1, "nombre" => "Zapatillas Nike", "precio" => 60),
+    array("id" => 2, "nombre" => "Sudadera Domyos", "precio" => 15),
+    array("id" => 3, "nombre" => "Pala de pádel Vairo", "precio" => 50),
+    array("id" => 4, "nombre" => "Pelota de baloncesto Molten", "precio" => 20)
+);
+
+if (!isset($_SESSION['carrito'])) $_SESSION['carrito'] = ["total" => 0, "items" => [], "cantidad" => []];
+
+function add_to_carrito($identificador) : void{
+    global $articulos;
+    $_SESSION["carrito"]["total"] += $articulos[$identificador]["precio"];
+    if (!in_array($articulos[$identificador]["nombre"],$_SESSION["carrito"]["items"])) {
+        $_SESSION["carrito"]["items"][] = $articulos[$identificador]["nombre"];
+        $_SESSION["carrito"]["cantidad"][] = 1;
+    } else{
+        $articuloEnCarrito = array_search($articulos[$identificador]["nombre"], $_SESSION["carrito"]["items"]);
+        if ($articuloEnCarrito !== false) {
+            $_SESSION["carrito"]["cantidad"][$articuloEnCarrito]++;
+        }
+    }
+}
+
+if (isset($_REQUEST["anyadir"])){
+    add_to_carrito(intval($_REQUEST["anyadir"]));
+}
+
+if (isset($_REQUEST["cerrar"])){
+    session_destroy();
+    header("Location: carro.php"); // Redirige a la página para que se actualice
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -6,29 +43,28 @@
     <link rel="stylesheet" type="text/css" href="/curso23-24/estilos/principal.css">
 </head>
 <body>
-<?php
-    $articulos = array(
-        "1" => array("id" => 1, "nombre" => "Zapatillas Nike", "precio" => 60),
-        array("id" => 2, "nombre" => "Sudadera Domyos", "precio" => 15),
-        array("id" => 3, "nombre" => "Pala de pádel Vairo", "precio" => 50),
-        array("id" => 4, "nombre" => "Pelota de baloncesto Molten", "precio" => 20)
-    );
-
-    $carrito = ["total" => 0, array()];
-
-    function add_to_carrito($identificador){
-        global $carrito, $articulos;
-        $carrito["total"] += $articulos[0]["precio"];
-        echo $carrito["total"];
-    }
-
-?>
 <main>
-        <button id="1" onclick="<?=add_to_carrito(0)?>">Zapatillas Nike (60 euros)</button>
-        <button id="2">Sudadera Domyos (15 euros)</button>
-        <button id="3">Pala de pádel Vairo (50 euros)</button>
-        <button id="4">Pelota de baloncesto Molten (20 euros)</button>
+    <?php
+        echo "Total: " . $_SESSION["carrito"]["total"] . "€";
+        echo "<br>Productos: <br>";
+        foreach ($_SESSION["carrito"]["items"] as $index => $item) {
+            echo $item;
+            if ($_SESSION["carrito"]["cantidad"][intval($index)] > 1){
+                echo " x ". $_SESSION["carrito"]["cantidad"][intval($index)];
+            }
+            echo " (" . $articulos[array_search($item, $_SESSION["carrito"]["items"])]["precio"] *
+                intval($_SESSION["carrito"]["cantidad"][$index]) . "€)";
+
+            echo "<br>";
+        }
+    ?>
+    <form action="carro.php" method="post">
+        <button name="anyadir" value="0">Zapatillas Nike (60 euros)</button>
+        <button name="anyadir" value="1">Sudadera Domyos (15 euros)</button>
+        <button name="anyadir" value="2">Pala de pádel Vairo (50 euros)</button>
+        <button name="anyadir" value="3">Pelota de baloncesto Molten (20 euros)</button>
+        <button name="cerrar" value="true">Vaciar carro</button>
+    </form>
 </main>
 </body>
 </html>
-
