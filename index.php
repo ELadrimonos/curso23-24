ffb8f433d838fb2c4a0588857148f8d389c00b81
Adrián Primo <adrpriber@alu.edu.gva.es>
2023-10-19 15:58:22 +0200

    Index Reorganizado

--- index.php
+++ index.php
@@ -18,21 +18,38 @@ $any = "2004";
 </header>
 <section>
     <article>
-        <h2>Actividades</h2>
+        <h2>Actividades Principio</h2>
         <p><a href="tablasmultiplicacion.php">Tablas de Multiplicación</a></p>
         <p><a href="primos.php">Numeros Primos</a></p>
         <p><a href="coches.php">Array Coches</a></p>
+    </article>
+
+    <article>
+        <h2>Actividades Arrays</h2>
         <p><a href="./Arrays/index.html">Arrays</a></p>
         <p><a href="./Apuestas/selec_apuesta.html">Apuestas</a></p>
         <p><a href="./ejercicios4/libros/form_libros.php">Libros</a></p>
         <p><a href="./ejercicios4/departamentos/form_dep.php">Departamentos</a></p>
         <p><a href="./ejercicios4/ley_dhondt/ley.php">Ley d'Hont</a></p>
+    </article>
+
+    <article>
+        <h2>Actividades Redirecciones, Cookies y Sesiones</h2>
         <p><a href="./Redirecciones/ejercicio1/formulario.php">Redirección 1</a></p>
         <p><a href="./Redirecciones/ejercicio2/formulario.php">Redirección 2</a></p>
         <p><a href="./ejercicios6/preferencias/preferencias.php">Preferencias Cookies</a></p>
         <p><a href="./ejercicios6/ficheros/ficheros.php">Contador fichero</a></p>
         <p><a href="./ejercicios6/carro/carro.php">Sesión Carrito Compra</a></p>
         <p><a href="./ejercicios6/login/index.php">Sesión Usuarios</a></p>
+    </article>
+
+    <article>
+        <h2>Actividades Clases/Objetos</h2>
+        <p><a href="./Ludopatia/ejercicio1.php">Ludopatía 1</a></p>
+        <p><a href="./Ludopatia/ejercicio2.php">Ludopatía 2</a></p>
+        <p><a href="./Ludopatia/ejercicio3.php">Ludopatía 3</a></p>
+        <p><a href="./Ludopatia/ejercicio4.php">Ludopatía 4</a></p>
+        <p><a href="./Terrestre/index.php">Vehículos Terrestres</a></p>
     </article>
 
     <article>
