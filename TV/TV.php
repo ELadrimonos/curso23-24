3bd3338d6aaf8509ec5e998a85a75395397210c2
Adrián Primo <adrpriber@alu.edu.gva.es>
2023-10-24 21:38:28 +0200

    TV 1

---
+++ TV/TV.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TV;
+
+class TV
+{
+
+}
---
+++ TV/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="/curso23-24/estilos/principal.css">
+</head>
+<body>
+<?php
+?>
+</body>
+</html>
+
