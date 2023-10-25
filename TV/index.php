39b24e159a9d37b60ab1e7451443a7d40c4bfd29
Adrián Primo <adrpriber@alu.edu.gva.es>
2023-10-25 20:51:56 +0200

    TV finalizada

--- TV/index.php
+++ TV/index.php
@@ -4,18 +4,33 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 session_start();
-if (!isset($_SESSION['tele'])) $_SESSION['tele'] = new TV("Samsung");
 
-if (isset($_POST["subirCanal"])){
-    $_SESSION['tele']->CambiarCanal($_SESSION['tele']->getCanal()+1);
-}else if (isset($_POST["bajarCanal"])){
-    $_SESSION['tele']->CambiarCanal($_SESSION['tele']->getCanal()-1);
-}else if (isset($_POST["subirVolumen"])){
+if (!isset($_SESSION['tele'])) {
+    $_SESSION['tele'] = new TV("Samsung");
+}
+
+if (isset($_POST["subirCanal"])) {
+    $_SESSION['tele']->CambiarCanal($_SESSION['tele']->getCanal() + 1);
+} elseif (isset($_POST["bajarCanal"])) {
+    $_SESSION['tele']->CambiarCanal($_SESSION['tele']->getCanal() - 1);
+} elseif (isset($_POST["subirVolumen"])) {
     $_SESSION['tele']->AumentarVolumen();
-}else if (isset($_POST["bajarVolumen"])){
+} elseif (isset($_POST["bajarVolumen"])) {
     $_SESSION['tele']->DisminuirVolumen();
-}else if (isset($_POST["reiniciar"])){
+} elseif (isset($_POST["reiniciar"])) {
     $_SESSION['tele']->Reiniciar();
+}
+
+if (!isset($_SESSION['canales'])) {
+    $_SESSION['canales'] = [];
+}
+
+$canalActual = $_SESSION['tele']->getCanal();
+if (isset($_SESSION['canales'][$canalActual])) {
+    $colorFondo = $_SESSION['canales'][$canalActual];
+} else {
+    $colorFondo = 'rgb(' . rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255) . ')';
+    $_SESSION['canales'][$canalActual] = $colorFondo;
 }
 
 ?>
@@ -30,49 +45,36 @@ if (isset($_POST["subirCanal"])){
 </head>
 <body>
 <div id="tele">
-    <div id="pantalla">
-
-    </div>
+    <div id="pantalla" style="background-color: <?= $colorFondo; ?>;"></div>
     <?php
     $television =  $_SESSION['tele'];
     echo "<h1>" . $television->getMarca() ."</h1>";
     ?>
-    </div>
-    <h1><?=$television->Estado()?></h1>
+</div>
+<h1><?=$television->Estado()?></h1>
 
-    <div id="Mando">
-        <form method="post">
-            <input type="submit" id="Apagado" name="reiniciar" value="X">
-            <div>
+<div id="Mando">
+    <form method="post">
+        <input type="submit" id="Apagado" name="reiniciar" value="X">
+        <div>
             <input type="submit" value="+" name="subirCanal">
             <input type="submit" value="-" name="bajarCanal">
-            </div>
-            <div>
+        </div>
+        <div>
             <input type="submit" value="+" name="subirVolumen">
             <input type="submit" value="-" name="bajarVolumen">
-            </div>
-        </form>
-    </div>
+        </div>
+        <input type="hidden" name="colorPantalla">
+    </form>
+</div>
 
-    <input type="range" list="markers" min="0" max="100" readonly disabled value="<?=$television->getVolumen()?>">
+<input type="range" list="markers" min="0" max="100" readonly disabled value="<?=$television->getVolumen()?>">
 <datalist id="markers">
-        <option value="0"></option>
-        <option value="25"></option>
-        <option value="50"></option>
-        <option value="75"></option>
-        <option value="100"></option>
-    </datalist>
-<script>
-    var pantalla = document.getElementById("pantalla");
-    var color = Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256);
-    var color1 = Math.floor(Math.random() * 256);
-    var color2 = Math.floor(Math.random() * 256);
-    var color3 = Math.floor(Math.random() * 256);
-    color = 'rgb(' + color1 + "," + color2 + "," + color3 + ');';
-    console.log(color)
-    pantalla.style.backgroundColor = color;
-    console.log(pantalla.style.background + " | " + color)
-</script>
+    <option value="0"></option>
+    <option value="25"></option>
+    <option value="50"></option>
+    <option value="75"></option>
+    <option value="100"></option>
+</datalist>
 </body>
 </html>
-
