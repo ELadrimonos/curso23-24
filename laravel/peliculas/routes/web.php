eae794bc0640cf682f6263089417b037a28edde8
Adrian Primo <adrpriber@alu.edu.gva.es>
2024-01-09 21:23:06 +0100

    Primer programa Laravel

---
+++ laravel/peliculas/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'Pantalla Principal';
+});
+
+Route::get('login', function () {
+    return 'login usuario';
+});
+
+Route::get('logout', function () {
+    return 'logout usuario';
+});
+
+Route::get('catalog', function () {
+    return 'Listado de peliculas';
+});
+
+
+// Quitando el valor por defecto de id, la pagina tiraria un error
+Route::get('catalog/show/{id?}', function ($id="?") {
+    return 'vista detalle película ' . $id;
+});
+
+Route::get('catalog/edit/{id?}', function ($id="?") {
+    return 'modificar película ' . $id;
+});
+
+Route::get('catalog/create', function () {
+    return 'Añadir';
+});
